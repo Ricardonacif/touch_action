4fd190e8b32918bfa7ee504f344c03701d741948
Ricardo Nacif <nacif.ricardo@gmail.com>
2014-10-15 01:21:03 -0300

    add press specs

--- spec/features/press_spec.rb
+++ spec/features/press_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe TouchAction do
-  describe "Doubletap" do
-    it 'should doubletap the div bound by HammerJS' do
+  describe "Press" do
+    it 'should press the div bound by HammerJS' do
       @browser.goto('localhost:9292/tap')
       element = @browser.div(id: 'myElement')
-      element.touch_action :doubletap
+      element.touch_action :press
       sleep(5)
-      expect(@browser.text).to include('doubletap')
+      expect(element.text).to include('press')
     end
   end
 end
