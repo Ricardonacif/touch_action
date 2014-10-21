f19420eb803ce8471e28266b1e09f33f35df83b1
Ricardo Nacif <rjunior@avenuecode.com>
2014-10-21 17:43:37 -0200

    add move specs

--- spec/features/flick_spec.rb
+++ spec/features/flick_spec.rb
@@ -4,37 +4,28 @@ RSpec.describe TouchAction do
   describe "Flick" do
     it 'should flick the div bound by hammerJs' do
       @browser.goto('http://localhost:9292')
-      div = @browser.div(id: 'blue')
+      div = @browser.div(id: 'hit')
       div.touch_action :flick
       sleep(5)
-      expect(@browser.p(id: 'direction').text).to include('swiped right')
+      expect(@browser.p(id: 'distance_x').text).to include('100')
     end
 
     it 'should flick the div bound by hammerJs when passing swipe instead of flick' do
       @browser.goto('http://localhost:9292')
-      div = @browser.div(id: 'blue')
+      div = @browser.div(id: 'hit')
       div.touch_action :swipe
       sleep(5)
-      expect(@browser.p(id: 'direction').text).to include('swiped right')
+      expect(@browser.p(id: 'distance_x').text).to include('100')
     end
 
     context "Options" do
-      it 'should flick the div bound by hammerJs 100px to the right as a default' do
-        @browser.goto('http://localhost:9292')
-        div = @browser.div(id: 'blue')
-        div.touch_action :flick
-        sleep(5)
-        expect(@browser.p(id: 'direction').text).to include('swiped right')
-        expect(@browser.p(id: 'duration').text).to include('100')
-      end
 
       it 'should flick the div bound by hammerJs to the left when passing the a negative distance' do
         @browser.goto('http://localhost:9292')
-        div = @browser.div(id: 'blue')
+        div = @browser.div(id: 'hit')
         div.touch_action :flick, distance: -80
         sleep(5)
-        expect(@browser.p(id: 'direction').text).to include('swiped left')
-        expect(@browser.p(id: 'duration').text).to include('80')
+        expect(@browser.p(id: 'distance_x').text).to include('-80')
       end
     end
   end
--- spec/features/move_spec.rb
+++ spec/features/move_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 RSpec.describe TouchAction do
   describe "Move" do
     it 'should move the div bound by hammerJs' do
-      binding.pry
       @browser.goto('http://localhost:9292')
-      div = @browser.div(id: 'blue')
-      div.touch_action :flick
+      div = @browser.div(id: 'hit')
+      div.touch_action :move
       sleep(5)
-      expect(@browser.p(id: 'direction').text).to include('swiped right')
+      expect(@browser.p(id: 'distance_x').text).to include('70')
+      expect(@browser.p(id: 'distance_y').text).to include('-50')
     end
 
   end
