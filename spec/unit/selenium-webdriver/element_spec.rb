c8a31897faf740e6fe0f967151c3006a8f0bae88
Ricardo Nacif <nacif.ricardo@gmail.com>
2015-01-04 22:51:19 -0200

    add selenium unit spec

---
+++ spec/unit/selenium-webdriver/element_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+RSpec.describe Selenium::WebDriver::Element do
+  describe "Selenium::WebDriver instance" do
+    it 'should respond to the method touch_action', use_selenium_webdriver: true do
+      @browser.navigate.to('http://localhost:9292')
+      element = @browser.find_element(:id, 'hit')
+      expect(element).to respond_to(:touch_action)
+    end
+  end
+end
